faa686132d580fc01f6a3833d47003ca0ba25a45
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-04-06 13:46:15 +0300

    фича: Добавлена работа справки

--- gendiff.js
+++ gendiff.js
@@ -1,4 +1,9 @@
-import { Command } from "commander";
+#!/usr/bin/env node
+import { program } from "commander";
 
+program
+  .name('genDiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
 
-export default gendiff;
+program.parse();
