cb5e84b7b5e6f55f3c0f6b454d7d10d4e915a6ed
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-10-23 23:45:44 +0300

    refactor: add gitingore from idea and delete excess code

--- src/parser.js
+++ src/parser.js
@@ -3,20 +3,13 @@ import path from 'path';
 import YAML from 'yaml';
 import { readFileSync } from 'fs';
 
-// const getAbsolutePath = (relativePath) => (relativePath[0] === '/' ? relativePath : path.resolve(relativePath));
-// const getFileExtension = (fileName) => (fileName.split('.').pop().toLowerCase()); // => 'yaml' || 'json'
-// const readFileContent = (filePath) => readFileSync(getAbsolutePath(filePath), 'utf-8');
-// const convertJsonToObject = (string) => JSON.parse(string);
-// const convertYamlToObject = (string) => YAML.parse(string);
-
 const parseFileToObject = (filePath) => {
   const allowedFormats = ['yml', 'yaml', 'json'];
   const fileExtension = filePath.split('.').pop().toLowerCase();
   if (allowedFormats.includes(fileExtension)) {
     const absolutePath = filePath[0] === '/' ? filePath : path.resolve(filePath);
     const fileContent = readFileSync(absolutePath, 'utf-8');
-    const obj = fileExtension === 'json' ? JSON.parse(fileContent) : YAML.parse(fileContent);
-    return obj;
+    return fileExtension === 'json' ? JSON.parse(fileContent) : YAML.parse(fileContent);
   }
   throw new Error('Only yml/yaml/json formats are allowed.');
 };
