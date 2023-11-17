fc71b2261da35f8f645ad391cf7bd8adb5ba10cd
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-11-17 20:39:34 +0200

    refactor: new comparator

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,10 +11,11 @@ const genDiff = (filepath1, filepath2, options) => {
 
   // Logic here
   const difference = compareObjects(originalObj, modifiedObj);
+  return JSON.stringify(difference, null ,2);
 
   // Formatting content
-  const format = options.formatType;
-  return formatDiff(originalObj, modifiedObj, difference, format);
+  // const format = options.formatType;
+  // return formatDiff(originalObj, modifiedObj, difference, format);
 };
 
 program
--- src/comparator.js
+++ src/comparator.js
@@ -1,45 +1,44 @@
-import isArray from 'lodash/isArray.js';
 import isObject from 'lodash/isObject.js';
 import union from 'lodash/union.js';
 import sortBy from 'lodash/sortBy.js';
-import reduce from 'lodash/reduce.js';
-
-function convertArrayToObject(arr) {
-  return reduce(
-    arr,
-    (acc, [key, value]) => {
-      if (isArray(value)) {
-        acc[key] = convertArrayToObject(value);
-      } else {
-        acc[key] = value;
-      }
-      return acc;
-    },
-    {},
-  );
-}
 
 export default function compareObjects(originalObj, modifiedObj) {
   const objsKeys = union(Object.keys(originalObj), Object.keys(modifiedObj));
   const sortedObjKeys = sortBy(objsKeys);
-  // Change this result to Array with data and status!!!
-  const arrayStructuredResult = sortedObjKeys.map((key) => {
+
+  return sortedObjKeys.map((key) => {
     if (isObject(originalObj[key]) && isObject(modifiedObj[key])) {
       const nestedDiff = compareObjects(originalObj[key], modifiedObj[key]);
-      return [key, nestedDiff];
+      return { key: nestedDiff };
     }
 
     if (!(key in modifiedObj)) {
-      return [key, 'deleted']; // deleted
+      return {
+        key,
+        changes: 'deleted',
+        value: originalObj[key],
+      }; // deleted
     }
 
     if (!(key in originalObj)) {
-      return [key, 'added']; // added
+      return {
+        key,
+        changes: 'added',
+        value: modifiedObj[key],
+      }; // added
     }
 
     return originalObj[key] !== modifiedObj[key]
-      ? [key, 'modified'] // modified
-      : [key, 'not changed']; // not changed
+      ? {
+        key,
+        changes: 'modified',
+        value: originalObj[key],
+        modifiedValue: modifiedObj[key],
+      } // modified
+      : {
+        key,
+        changes: 'not changed',
+        value: modifiedObj[key],
+      }; // not changed
   });
-  return convertArrayToObject(arrayStructuredResult);
 }
