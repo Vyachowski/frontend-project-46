a6373dd871ca45f897ac6c16f6da6cf137b6fbdb
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-11-18 04:43:05 +0200

    refactor: added proper file extension checker for parser.js

--- src/parser.js
+++ src/parser.js
@@ -4,12 +4,12 @@ import YAML from 'yaml';
 import { readFileSync } from 'fs';
 
 const parseFileToObject = (filePath) => {
-  const allowedFormats = ['yml', 'yaml', 'json'];
-  const fileExtension = filePath.split('.').pop().toLowerCase();
+  const allowedFormats = ['.yml', '.yaml', '.json'];
+  const fileExtension = path.extname(filePath);
   if (allowedFormats.includes(fileExtension)) {
     const absolutePath = filePath.startsWith('/') ? filePath : path.resolve(filePath);
     const fileContent = readFileSync(absolutePath, 'utf-8');
-    return fileExtension === 'json' ? JSON.parse(fileContent) : YAML.parse(fileContent);
+    return fileExtension === '.json' ? JSON.parse(fileContent) : YAML.parse(fileContent);
   }
   throw new Error('Only yml/yaml/json formats are allowed. Please try again');
 };
