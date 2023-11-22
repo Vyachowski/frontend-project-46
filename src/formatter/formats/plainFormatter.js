72b6ddfb41265b514d10b670ea1581d17517ffbb
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-11-22 22:25:52 +0200

    feat: remove excess comments

--- src/formatter/formats/plainFormatter.js
+++ src/formatter/formats/plainFormatter.js
@@ -20,12 +20,10 @@ export default function plainFormatter(difference) {
     }
     return '';
   };
-  // eslint-disable-next-line consistent-return
+
   const iter = (currentValue, path = '') => {
     // eslint-disable-next-line no-prototype-builtins
     if (currentValue.hasOwnProperty('value') && !isObject(currentValue.value)) {
-      // console.log(currentValue.hasOwnProperty('value'), isObject(currentValue.value));
-      // console.log('Edge case called\n');
       const { changes, value, updatedValue } = currentValue;
       if (!(['not changed'].includes(currentValue.changes))) {
         const stringEnd = defineStringEnd(changes, value, updatedValue);
@@ -35,22 +33,17 @@ export default function plainFormatter(difference) {
     }
 
     const entries = Object.entries(currentValue);
-    // console.log('Not edge case called\n');
     const lines = entries.map(([key, diff]) => {
       const property = path ? `${path}.${key}` : key;
 
       switch (diff.changes) {
         case 'added':
-          // console.log('Added called');
           return isObject(diff.value) ? `Property '${property}' was added with value: ${formatValue(diff.value)}${iter(diff.value, key)}` : `Property '${property}' was added with value: ${formatValue(diff.value)}`;
         case 'removed':
-          // console.log('Removed called');
           return isObject(diff.value) ? `Property '${property}' was removed${iter(diff.value, key)}` : `Property '${property}' was removed`;
         case 'updated':
-          // console.log('Updated called');
           return isObject(diff.value) ? `Property '${property}' was updated. From ${formatValue(diff.value)} to ${formatValue(diff.updatedValue)}${iter(diff.value, key)}` : `Property '${property}' was updated. From ${formatValue(diff.value)} to ${formatValue(diff.updatedValue)}`;
         default:
-          // console.log('Default called');
           return isObject(diff) ? `${iter(diff, property)}` : 'empty entity';
       }
     });
