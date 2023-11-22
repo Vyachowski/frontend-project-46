fa73cb7aa498721bf7f2ad13a8771eb55862c3a4
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-11-22 18:40:43 +0200

    feat: finish plain formatter

--- src/formatter/formats/plainFormatter.js
+++ src/formatter/formats/plainFormatter.js
@@ -1,18 +1,29 @@
 import isObject from 'lodash/isObject.js';
 
 export default function plainFormatter(difference) {
+  const formatValue = (value) => {
+    if (isObject(value)) {
+      return '[complex value]';
+    }
+    if (typeof value === 'string') {
+      return `'${value}'`;
+    }
+    return value;
+  };
   // eslint-disable-next-line consistent-return
   const iter = (currentValue, path = '') => {
     // eslint-disable-next-line no-prototype-builtins
     if (currentValue.hasOwnProperty('value') && !isObject(currentValue.value)) {
+      // console.log(currentValue.hasOwnProperty('value'), isObject(currentValue.value));
+      // console.log('Edge case called\n');
       const { changes, value, updatedValue } = currentValue;
-      if (!(['not changed', undefined].includes(currentValue.changes))) {
+      if (!(['not changed'].includes(currentValue.changes))) {
         let stringEnd = '';
         if (changes === 'added') {
-          stringEnd = ` with value: ${isObject(value) ? '[complex value]' : value}`;
+          stringEnd = ` with value: ${formatValue(value)}`;
         }
         if (changes === 'updated') {
-          stringEnd = `. From ${isObject(value) ? '[complex value]' : value} to ${isObject(updatedValue) ? '[complex value]' : updatedValue}`;
+          stringEnd = `. From ${formatValue(value)} to ${formatValue(updatedValue)}`;
         }
         return `Property ${path} was ${changes}${stringEnd}`;
       }
@@ -20,24 +31,28 @@ export default function plainFormatter(difference) {
     }
 
     const entries = Object.entries(currentValue);
-    // console.log('Current Value is\n', currentValue);
+    // console.log('Not edge case called\n');
     const lines = entries.map(([key, diff]) => {
       const property = path ? `${path}.${key}` : key;
 
       switch (diff.changes) {
         case 'added':
-          return isObject(diff.value) ? `Property '${property}' was added with value: ${diff.value}\n${iter(diff.value, key)}` : `Property '${property}' was added with value: ${diff.value}`;
+          // console.log('Added called');
+          return isObject(diff.value) ? `Property '${property}' was added with value: ${formatValue(diff.value)}${iter(diff.value, key)}` : `Property '${property}' was added with value: ${formatValue(diff.value)}`;
         case 'removed':
-          return isObject(diff.value) ? `Property '${property}' was removed\n${iter(diff.value, key)}` : `Property '${property}' was removed`;
+          // console.log('Removed called');
+          return isObject(diff.value) ? `Property '${property}' was removed${iter(diff.value, key)}` : `Property '${property}' was removed`;
         case 'updated':
-          return isObject(diff.value) ? `Property '${property}' was updated. From ${diff.value} to ${diff.updatedValue}\n${iter(diff.value, key)}` : `Property '${property}' was updated. From ${diff.value} to ${diff.updatedValue}`;
+          // console.log('Updated called');
+          return isObject(diff.value) ? `Property '${property}' was updated. From ${formatValue(diff.value)} to ${formatValue(diff.updatedValue)}${iter(diff.value, key)}` : `Property '${property}' was updated. From ${formatValue(diff.value)} to ${formatValue(diff.updatedValue)}`;
         default:
-          return isObject(diff.value) ? `${iter(diff.value, key)}` : 'empty entity';
+          // console.log('Default called');
+          return isObject(diff) ? `${iter(diff, property)}` : 'empty entity';
       }
     });
 
     return [
-      ...lines.flat(),
+      ...lines.flat().filter((value) => value !== 'empty entity'),
     ].join('\n');
   };
 
