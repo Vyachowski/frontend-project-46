1be4c2f97c51f661345e47fc2b1ce4280d7f540d
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-11-22 13:39:16 +0200

    refactor: remove excess comment

--- src/formatter/index.js
+++ src/formatter/index.js
@@ -4,7 +4,6 @@ import plainFormatter from './formats/plainFormatter.js';
 export default function formatDiff(difference, format) {
   switch (format) {
     case 'plain':
-      // console.log('Plain was runned!', JSON.stringify(difference, null, 2));
       return console.log(plainFormatter(difference));
     default:
       return console.log(stylishFormatter(difference));
