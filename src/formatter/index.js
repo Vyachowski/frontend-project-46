c7fbc8216199127a6f9abd8fd66e8bbd274a6b9a
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-11-22 18:47:00 +0200

    feat: json formatter

--- src/formatter/index.js
+++ src/formatter/index.js
@@ -1,8 +1,11 @@
 import stylishFormatter from './formats/stylishFormatter.js';
 import plainFormatter from './formats/plainFormatter.js';
+import JSONFormatter from './formats/JSONFormatter.js';
 
 export default function formatDiff(difference, format) {
   switch (format) {
+    case 'json':
+      return console.log(JSONFormatter(difference));
     case 'plain':
       return console.log(plainFormatter(difference));
     default:
