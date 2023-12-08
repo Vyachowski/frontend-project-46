e7c4a50758758e1c4a303fb1d39ca795949b5bd1
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-12-08 23:25:08 +0200

    refactor: alternate version for buildAST

--- src/buildAST.js
+++ src/buildAST.js
@@ -3,24 +3,51 @@ import union from 'lodash/union.js';
 import sortBy from 'lodash/sortBy.js';
 import has from 'lodash/has.js';
 
+// export default function buildAST(originalObj, modifiedObj) {
+//   const objsKeys = union(Object.keys(originalObj), Object.keys(modifiedObj));
+//   const sortedObjKeys = sortBy(objsKeys);
+//   return sortedObjKeys.map((key) => {
+//     const originalValue = originalObj[key];
+//     const modifiedValue = modifiedObj[key];
+//     if (!has(originalObj, key)) return { key, type: 'added', modifiedValue };
+//     if (!has(modifiedObj, key)) return { key, type: 'removed', originalValue };
+//     if (isObject(originalValue) && isObject(modifiedValue)) {
+//       return { key, type: 'nested', children: buildAST(originalValue, modifiedValue) };
+//     }
+//     if (originalValue !== modifiedValue) {
+//       return {
+//         key, type: 'updated', originalValue, modifiedValue,
+//       };
+//     }
+//     return {
+//       key, type: 'unchanged', originalValue,
+//     };
+//   });
+// }
+
+// Alternative version
 export default function buildAST(originalObj, modifiedObj) {
   const objsKeys = union(Object.keys(originalObj), Object.keys(modifiedObj));
-  const sortedObjKeys = sortBy(objsKeys);
-  return sortedObjKeys.map((key) => {
+  return sortBy(objsKeys).map((key) => {
     const originalValue = originalObj[key];
     const modifiedValue = modifiedObj[key];
-    if (!has(originalObj, key)) return { key, type: 'added', modifiedValue };
-    if (!has(modifiedObj, key)) return { key, type: 'removed', originalValue };
-    if (isObject(originalValue) && isObject(modifiedValue)) {
-      return { key, type: 'nested', children: buildAST(originalValue, modifiedValue) };
-    }
-    if (originalValue !== modifiedValue) {
-      return {
-        key, type: 'updated', originalValue, modifiedValue,
+    const node = { key };
+    let resultNode;
+
+    if (!has(originalObj, key)) {
+      resultNode = { ...node, type: 'added', modifiedValue };
+    } else if (!has(modifiedObj, key)) {
+      resultNode = { ...node, type: 'removed', originalValue };
+    } else if (isObject(originalValue) && isObject(modifiedValue)) {
+      resultNode = { ...node, type: 'nested', children: buildAST(originalValue, modifiedValue) };
+    } else if (originalValue !== modifiedValue) {
+      resultNode = {
+        ...node, type: 'updated', originalValue, modifiedValue,
       };
+    } else {
+      resultNode = { ...node, type: 'unchanged', originalValue };
     }
-    return {
-      key, type: 'unchanged', originalValue,
-    };
+
+    return resultNode;
   });
 }
