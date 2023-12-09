4e1ff06f1a290e5be85541c0bfeb79c84a7d769c
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-12-09 14:22:01 +0200

    refactor: break code into small functions

--- src/formatter/formats/stylishFormatter.js
+++ src/formatter/formats/stylishFormatter.js
@@ -1,35 +1,43 @@
 import isObject from 'lodash/isObject.js';
 
+const spacer = ' '.repeat(4);
+
+function getIndentation(depth, symbol = '') {
+  const indentation = spacer.repeat(depth);
+  return (symbol === '' ? indentation : `${indentation.slice(2)}${symbol} `);
+}
+
+function getBracketIndentation(depth) {
+  return spacer.repeat(depth - 1);
+}
+
 function stringify(value, depth) {
   if (!isObject(value)) {
     return value;
   }
-  const indentation = '    '.repeat(depth);
-  const bracketIndentation = '    '.repeat(depth - 1);
-  const lines = Object.entries(value).map(([key, val]) => `${indentation}${key}: ${stringify(val, depth + 1)}`);
-  return ['{', ...lines, `${bracketIndentation}}`].join('\n');
+  const lines = Object.entries(value).map(
+    ([key, val]) => `${getIndentation(depth)}${key}: ${stringify(val, depth + 1)}`,
+  );
+  return ['{', ...lines, `${getBracketIndentation(depth)}}`].join('\n');
 }
 
 export default function stylishFormatter(difference, depth = 1) {
-  const indentation = '    '.repeat(depth);
-  const shortIndentation = indentation.slice(2);
-  const bracketIndentation = '    '.repeat(depth - 1);
   const nextDepth = depth + 1;
   const lines = difference.map((node) => {
     let line;
     const { key, type, children } = node;
     if (type === 'nested') {
-      line = `${indentation}${key}: ${stylishFormatter(children, nextDepth)}`;
+      line = `${getIndentation(depth)}${key}: ${stylishFormatter(children, nextDepth)}`;
     } else if (type === 'added') {
-      line = `${shortIndentation}+ ${key}: ${stringify(node.modifiedValue, nextDepth)}`;
+      line = `${getIndentation(depth, '+')}${key}: ${stringify(node.modifiedValue, nextDepth)}`;
     } else if (type === 'removed') {
-      line = `${shortIndentation}- ${key}: ${stringify(node.originalValue, nextDepth)}`;
+      line = `${getIndentation(depth, '-')}${key}: ${stringify(node.originalValue, nextDepth)}`;
     } else if (type === 'updated') {
-      line = `${shortIndentation}- ${key}: ${stringify(node.originalValue, nextDepth)}\n${shortIndentation}+ ${key}: ${stringify(node.modifiedValue, nextDepth)}`;
+      line = `${getIndentation(depth, '-')}${key}: ${stringify(node.originalValue, nextDepth)}\n${getIndentation(depth, '+')}${key}: ${stringify(node.modifiedValue, nextDepth)}`;
     } else {
-      line = `${indentation}${key}: ${node.originalValue}`;
+      line = `${getIndentation(depth)}${key}: ${node.originalValue}`;
     }
     return line;
   });
-  return `{\n${lines.join('\n')}\n${bracketIndentation}}`;
+  return `{\n${lines.join('\n')}\n${getBracketIndentation(depth)}}`;
 }
