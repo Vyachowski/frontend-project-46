1a08dc97858f5df0040bb3456a073c23bba2d8bb
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-12-20 19:04:28 +0200

    refactor: delete excess comments

--- src/buildAST.js
+++ src/buildAST.js
@@ -3,29 +3,6 @@ import union from 'lodash/union.js';
 import sortBy from 'lodash/sortBy.js';
 import has from 'lodash/has.js';
 
-// export default function buildAST(originalObj, modifiedObj) {
-//   const objsKeys = union(Object.keys(originalObj), Object.keys(modifiedObj));
-//   const sortedObjKeys = sortBy(objsKeys);
-//   return sortedObjKeys.map((key) => {
-//     const originalValue = originalObj[key];
-//     const modifiedValue = modifiedObj[key];
-//     if (!has(originalObj, key)) return { key, type: 'added', modifiedValue };
-//     if (!has(modifiedObj, key)) return { key, type: 'removed', originalValue };
-//     if (isObject(originalValue) && isObject(modifiedValue)) {
-//       return { key, type: 'nested', children: buildAST(originalValue, modifiedValue) };
-//     }
-//     if (originalValue !== modifiedValue) {
-//       return {
-//         key, type: 'updated', originalValue, modifiedValue,
-//       };
-//     }
-//     return {
-//       key, type: 'unchanged', originalValue,
-//     };
-//   });
-// }
-
-// Alternative version
 export default function buildAST(originalObj, modifiedObj) {
   const objsKeys = union(Object.keys(originalObj), Object.keys(modifiedObj));
   return sortBy(objsKeys).map((key) => {
